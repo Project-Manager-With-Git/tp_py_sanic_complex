cea1c5a1ea5549ed8bf2161b3b60fde7b28262e2
HUANG SIZHE <hsz1273327@gmail.com>
2021-06-11 16:39:58 +0800

    Update __init__.py

--- test_sanic/apis/__init__.py
+++ test_sanic/apis/__init__.py
@@ -1,9 +1,18 @@
 from sanic import Sanic, Blueprint
+from sanic.request import Request
+from sanic.response import HTTPResponse, text
+from sanic_openapi import doc
 from .usernamespace import usernamespace
 
 api = Blueprint.group(usernamespace, url_prefix="/api")
 
 
 def init_api(app: Sanic) -> Sanic:
+    app.get("/ping")(ping)
     app.blueprint(api)
     return app
+
+
+@doc.summary("ping接口用于健康检查")
+async def ping(request: Request) -> HTTPResponse:
+    return text("pong")
